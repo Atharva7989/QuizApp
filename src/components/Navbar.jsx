899fb049a58a3e448ccf25150ff59bbf119bed76
unknown <atharvadonkar12@gmail.com>
2024-11-29 13:21:48 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="m-4 h-[60px] bg-white">
+        <div className="flex items-center justify-center">
+            <h1>Firebase App</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
